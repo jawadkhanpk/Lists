9903fd9ff9cbe129e03e8800f22f066d7f30249f
jawadkhanpk <jawadkhan.tech@gmail.com>
2022-01-10 18:47:58 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey",
+                     "Georgia", "Connecticut", "Massachusetts",
+                     "Maryland", "South Carolina", "New Hampshire",
+                     "Virginia", "New York", "North Carolina", "Rhode Island",
+                     "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana",
+                     "Indiana", "Mississippi", "Illinois", "Alabama", "Maine",
+                     "Missouri", "Arkansas", "Michigan", "Florida", "Texas", "Iowa",
+                     "Wisconsin", "California", "Minnesota", "Oregon", "Kansas",
+                     "West Virginia", "Nevada", "Nebraska", "Colorado", "North Dakota",
+                     "South Dakota", "Montana", "Washington", "Idaho", "Wyoming",
+                     "Utah", "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"]
+
+states_of_america[1] = "Pencilvania"        # TO Edit any item present in the list
+
+states_of_america.append("Jawadland")       # To Add item at the end of a list
+
+
+states_of_america.extend(["Hammad Land", "Adan Land", "Danial Land"])
+print(states_of_america)
